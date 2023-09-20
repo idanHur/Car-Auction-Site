b7f5e3fd2178f5a09cb6cb9092d600edb98fd0b3
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-20 17:05:33 +0300

    Added db initializer

---
+++ src/AuctionService/Data/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class DbInitializer
+{
+    public static void InitDb(WebApplication app){
+        using var scope = app.Services.CreateScope();
+
+        SeedData(scope.ServiceProvider.GetService<AuctionDbContext>());
+    }
+    private static void SeedData(AuctionDbContext context)
+    {
+        context.Database.Migrate();
+
+        if(context.Auctions.Any())
+        {
+            Console.WriteLine("Already have data - no need to seed");
+            return;
+        }
+
+        var auctions = new List<Auction>()
+        {
+            //Auctions
+        };
+
+        context.AddRange(auctions);
+        context.SaveChanges();
+    }
+}
