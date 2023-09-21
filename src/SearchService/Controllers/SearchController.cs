6bd56d3edf2001863faffb6298b660a461f95e51
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-22 00:23:18 +0300

    Added endpoint to search controller

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
 
 namespace SearchService;
 
@@ -6,5 +8,18 @@ namespace SearchService;
 [Route("api/search")]
 public class SearchController : ControllerBase
 {
-    
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    {
+        var query = DB.Find<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+        var result = await query.ExecuteAsync();
+        return result;
+    }
 }
