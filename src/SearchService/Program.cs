9301f1706cc81bbe4c18ea66060f80030172003d
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-22 17:30:39 +0300

    Added http communication between services

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -2,11 +2,12 @@ using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService;
 using SearchService.Models;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 // Add services to the container.
 var app = builder.Build();
 
