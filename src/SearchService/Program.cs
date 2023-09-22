9198da6fd0ef9eff5ba1de84f059cfb1ea162258
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-22 18:00:26 +0300

    Added resilience to http communication

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,13 +1,14 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
-using SearchService.Models;
 using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
 // Add services to the container.
 var app = builder.Build();
 
@@ -16,13 +17,22 @@ var app = builder.Build();
 app.UseAuthorization();
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitiallizer.InitDb(app);
-}
-catch(Exception e)
-{
-    Console.WriteLine(e);
-}
+    try
+    {
+        await DbInitiallizer.InitDb(app);
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
