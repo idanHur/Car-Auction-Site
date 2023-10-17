a728c93315072cd7565541fd226fb390d893c7b8
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-10-17 20:34:58 +0300

    Added signalR functionality to auction details

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -29,10 +29,10 @@ export default function SignalRProvider({children}: Props) {
                 .then(() => {
                     console.log('Connected to notifications hub');
                     connection.on('BidPlaced', (bid: Bid) => {
-                        console.log('Bid placed event recived');
                         if(bid.bidStatus.includes('Accepted')){
                             setCurrentPrice(bid.auctionId, bid.amount);
                         }
+                        addBid(bid);
                     })
                 }).catch(error => console.log(error));
         }
